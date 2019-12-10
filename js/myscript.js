d60ede747aa475e6a3b8e94d33b47bf7bb812261
Jim Reed <cjoackin@gmail.com>
2019-12-10 05:10:09 -0300

    Added bottom padding

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+/*
+$('document').ready(function(){
+	setTimeout( function () {
+		$('#reg').hide();
+	}, 3000);
+});
+
+$('#reg').hover(
+	function() {
+		$(this).show();
+	},
+	function() {
+		$(this).hide();
+	}
+);
+*/
